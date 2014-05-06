a7250513bbfd318e0197055eb84f4f3a52d7d425
Fotomen <student_pro@hotmail.com>
2014-05-06 13:25:14 +0200

    004
    W tej wersji STM poprawnie sprawdza hasło.

--- TestIO/kbd/kbd_4x4.c
+++ TestIO/kbd/kbd_4x4.c
@@ -31,22 +31,22 @@
 #define KBD_R1C1 0x31 //1
 #define KBD_R1C2 0x32 //2
 #define KBD_R1C3 0x33 //3
-#define KBD_R1C4 0x49 //A
+#define KBD_R1C4 0x41 //A
 
 #define KBD_R2C1 0x34 //4
 #define KBD_R2C2 0x35 //5
 #define KBD_R2C3 0x36 //6
-#define KBD_R2C4 0x51 //B
+#define KBD_R2C4 0x42 //B
 
 #define KBD_R3C1 0x37 //7
 #define KBD_R3C2 0x38 //8
 #define KBD_R3C3 0x39 //9
-#define KBD_R3C4 0x55 //C
+#define KBD_R3C4 0x43 //C
 
-#define KBD_R4C1 0x2C //*
-#define KBD_R4C2 0x40 //0
-#define KBD_R4C4 0x53 //D
-#define KBD_R4C3 0x54 //#
+#define KBD_R4C1 0x2A //*
+#define KBD_R4C2 0x30 //0
+#define KBD_R4C4 0x44 //D
+#define KBD_R4C3 0x23 //#
 
 const char keyMap[4][4]={{KBD_R1C1, KBD_R1C2, KBD_R1C3, KBD_R1C4},
                          {KBD_R2C1, KBD_R2C2, KBD_R2C3, KBD_R2C4},
--- TestIO/main.c
+++ TestIO/main.c
@@ -1,40 +1,101 @@
 #include "stm32f4xx_rcc.h"
 #include "lcd_hd44780.h"
 #include "kbd_4x4.h"
-
-
+#include "string.h"
 int main(void)
 {
 	unsigned char Tekst[2] = {"\0"};
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);  //LCD KBD
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);  //KBD
+	KBD_Init();
+	LCD_Initialize();                           //inicjalizacja wysietlacza
+	LCD_WriteCommand(HD44780_CLEAR);            //wyczysc wyswietlacz
 
 
-	 RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);  //LCD KBD
-	 RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);  //KBD
+	x:
+	LCD_WriteText((unsigned char *)"Podaj ID: \0");
+	LCD_GoTo(0,1);
 
-	  KBD_Init();
-	  LCD_Initialize();                           //inicjalizacja wysietlacza
-	  LCD_WriteCommand(HD44780_CLEAR);            //wyczysc wyswietlacz
+	int i=0;
+	char tab[5]={0,0,0,0,0};
 
+	//1
+	Tekst[0]=0;
+	Tekst[1]=0;
+	while (1)
+	{
+		Tekst[0]=KBD_ReadKey();
+		if (Tekst[0] !=0 )
+		{
+			tab[0]=Tekst[0];
+			break;
+		}
+	};
+	LCD_GoTo(0,1);
+	LCD_WriteText((unsigned char *)"*            ");
 
-	  x:
-	  LCD_WriteText((unsigned char *)"Podaj ID:[A/C] \0");
-	  //A-accept
-	  //C-cancel
-	  //goto nowa linia
-	  //****
-	  //if C -> goto x;
-	  //if A -> check id
-	  //true -> Podaj pin
-	  //false -> "bledne id"
-	  //true -> "Otworz drzwi"
-	  //false -> "Bledny pin"
+	//2
+	Tekst[0]=0;
+	Tekst[1]=0;
+	while (1)
+	{
+		Tekst[0]=KBD_ReadKey();
+		if (Tekst[0] !=0 )
+		{
+			tab[1]=Tekst[0];
+			break;
+		}
+	};
+	LCD_GoTo(0,1);
+	LCD_WriteText((unsigned char *)"**           ");
 
-	  goto x;
-	  while (1) {
-	    Tekst[0]=KBD_ReadKey();
-	    if (Tekst[0] !=0 ){
-	    	LCD_WriteTextXY(Tekst,9,0);
-	    }
-	  };
-	  return 0;
+	//3
+	Tekst[0]=0;
+	Tekst[1]=0;
+	while (1)
+	{
+		Tekst[0]=KBD_ReadKey();
+		if (Tekst[0] !=0 )
+		{
+			tab[2]=Tekst[0];
+			break;
+		}
+	};
+	LCD_GoTo(0,1);
+	LCD_WriteText((unsigned char *)"***          ");
+
+	//4
+	Tekst[0]=0;
+	Tekst[1]=0;
+	while (1)
+	{
+		Tekst[0]=KBD_ReadKey();
+		if (Tekst[0] !=0 )
+		{
+			tab[3]=Tekst[0];
+			break;
+		}
+	};
+	LCD_GoTo(0,1);
+	LCD_WriteText((unsigned char *)"***          ");
+	tab[4]=0;
+
+	unsigned char* haslo="1234\0";
+
+	if ( strcmp( tab, haslo )==0)
+	{
+		LCD_WriteCommand(HD44780_CLEAR);
+		LCD_GoTo(0,0);
+		LCD_WriteText((unsigned char *)"Haslo Poprawne");
+	}
+	else
+	{
+		LCD_WriteCommand(HD44780_CLEAR);
+		LCD_GoTo(0,0);
+		LCD_WriteText((unsigned char *)"Blad");
+	}
+
+	while(1);
+
+	return 0;
 }
